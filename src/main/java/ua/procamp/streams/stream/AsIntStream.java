450ef8f6cd2a661a5a148d9cc1fd0dab3112f2ba
va13ak <va13ak@gmail.com>
2019-02-27 18:59:29 +0200

    code cleaning

--- src/main/java/ua/procamp/streams/stream/AsIntStream.java
+++ src/main/java/ua/procamp/streams/stream/AsIntStream.java
@@ -3,7 +3,6 @@ package ua.procamp.streams.stream;
 import ua.procamp.streams.function.*;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class AsIntStream implements IntStream {
     protected ArrayList<Integer> arrayList;
@@ -11,21 +10,19 @@ public class AsIntStream implements IntStream {
     private AsIntStream() {
         arrayList = new ArrayList<>();
     }
-    
+
     public static IntStream of(int... values) {
         AsIntStream newStream = new AsIntStream();
-        for (int value: values
-             ) {
+        for (int value : values)
             newStream.arrayList.add(value);
-        }
         return newStream;
     }
 
     @Override
     public Double average() {
         Long cnt = count();
-        if (cnt == 0) throw new IllegalArgumentException("Stream is empty!");
-        return sum().doubleValue()/cnt.doubleValue();
+        if (cnt == 0) throw new IllegalArgumentException("Stream is empty");
+        return sum().doubleValue() / cnt.doubleValue();
     }
 
     @Override
@@ -54,60 +51,47 @@ public class AsIntStream implements IntStream {
     @Override
     public IntStream filter(IntPredicate predicate) {
         AsIntStream newStream = new AsIntStream();
-        for (int value: arrayList
-                ) {
+        for (int value : arrayList)
             if (predicate.test(value)) newStream.arrayList.add(value);
-        }
         return newStream;
     }
 
     @Override
     public void forEach(IntConsumer action) {
-        for (int value: arrayList
-                ) {
+        for (int value : arrayList)
             action.accept(value);
-        }
     }
 
     @Override
     public IntStream map(IntUnaryOperator mapper) {
         AsIntStream newStream = new AsIntStream();
-        for (int value: arrayList
-                ) {
+        for (int value : arrayList)
             newStream.arrayList.add(mapper.apply(value));
-        }
         return newStream;
     }
 
     @Override
     public IntStream flatMap(IntToIntStreamFunction func) {
         AsIntStream newStream = new AsIntStream();
-        for (int value: arrayList
-                ) {
-            for (int intValue: func.applyAsIntStream(value).toArray()
-                 ) {
+        for (int value : arrayList)
+            for (int intValue : func.applyAsIntStream(value).toArray())
                 newStream.arrayList.add(intValue);
-            }
-        }
         return newStream;
     }
 
     @Override
     public int reduce(int identity, IntBinaryOperator op) {
         int result = identity;
-        for (int value: arrayList
-                ) {
+        for (int value : arrayList)
             result = op.apply(result, value);
-        }
         return result;
     }
 
     @Override
     public int[] toArray() {
         int[] intArray = new int[arrayList.size()];
-        for (int i = 0; i < arrayList.size(); i++) {
+        for (int i = 0; i < arrayList.size(); i++)
             intArray[i] = arrayList.get(i);
-        }
         return intArray;
     }
 
